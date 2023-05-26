93f105748408eee6865c154f44900e32345dda0c
Olenteva74 <olenteva74@ukr.net>
2023-05-26 14:36:07 +0300

    Routes

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,18 @@
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom"; 
+import { AppBar } from "components/AppBar";
+import { Loader } from "components/Loader";
+import { Container } from "./Layout.styled";
+
+export const Layout = () => {
+    return (
+        <Container>
+          <AppBar/>
+          <main>
+            <Suspense fallback={<Loader/>}>
+                <Outlet/>
+            </Suspense>
+          </main>
+        </Container>
+    )
+}
